82d6e9356b21424059e672540b4be15c3f3ef1aa
Akash Kumar Parida <akashkumar_parida@epam.com>
2025-01-28 18:25:05 +0530

    qeml-2-task-8

---
+++ src/main/java/com/payrollmanagement/SalariesOfEmployeeList.java
@@ -0,0 +1,29 @@
+package com.payrollmanagement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SalariesOfEmployeeList {
+    public static void main(String[] args) {
+        List<Employee> employeeList = List.of(
+                new FullTimeEmployee(1,"Akash","IT",80000,10000),
+                new FullTimeEmployee(2,"Vikram","IT",70000,7000),
+                new FullTimeEmployee(3,"Suvarno","IT",50000,8000),
+                new FullTimeEmployee(4,"Nandi","IT",90000,3000)
+        );
+        List<Integer> salaries = employeeList.stream().map(e->e.getBaseSalary()).collect(Collectors.toList());
+        System.out.println(salaries);
+        if(employeeList.size()>1){
+            List<Employee> sortedEmployees = employeeList.stream().sorted((e1,e2)-> {
+                if(e1==null){
+                    return -1;
+                }
+                if(e2==null){
+                    return 1;
+                }
+                return Integer.compare(e1.getBaseSalary(), e2.getBaseSalary());
+            }).collect(Collectors.toList());
+            System.out.println(sortedEmployees);
+        }
+    }
+}
